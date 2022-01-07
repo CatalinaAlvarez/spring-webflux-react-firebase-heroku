9cabc33273a35a37f6c0acfb4881d33e02a87ba5
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2022-01-06 21:40:04 -0500

    Se añade el equals a userDto

--- back/demo-empresarial/src/main/java/co/com/sofka/questions/model/UserDTO.java
+++ back/demo-empresarial/src/main/java/co/com/sofka/questions/model/UserDTO.java
@@ -1,6 +1,7 @@
 package co.com.sofka.questions.model;
 
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
 public class UserDTO {
     private String id;
@@ -70,5 +71,18 @@ public class UserDTO {
 
     public void setPictureUrl(String pictureUrl) {
         this.pictureUrl = pictureUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDTO userDTO = (UserDTO) o;
+        return Objects.equals(id, userDTO.id) && Objects.equals(uid, userDTO.uid) && Objects.equals(name, userDTO.name) && Objects.equals(lastname, userDTO.lastname) && Objects.equals(email, userDTO.email) && Objects.equals(pictureUrl, userDTO.pictureUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, uid, name, lastname, email, pictureUrl);
     }
 }
