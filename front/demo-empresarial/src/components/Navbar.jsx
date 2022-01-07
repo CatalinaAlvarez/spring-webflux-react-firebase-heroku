a7cd8f5577a6e222da6283058b64405b533faf54
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2022-01-07 13:48:20 -0500

    Se añade el logo

--- front/demo-empresarial/src/components/Navbar.jsx
+++ front/demo-empresarial/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import {Link} from "react-router-dom"
+import logo from "../assets/logo.png"
 
 const Navbar = ({elements}) => {
     return (
         <nav>
         <section >
-           
+           <img className="logo" src={logo} alt="logo"/>
            {
                elements.map((element,index)=>{
                    return (<Link key={index} to={element.url}>{element.titulo}</Link>)
